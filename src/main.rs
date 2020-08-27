78bcfb09c8e340be6691892969bd909052938efa
Peter <mezeipetister@gmail.com>
2020-08-27 18:53:52 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -56,25 +56,79 @@ impl Customer for CustomerService {
         &self,
         request: Request<CreateNewRequest>,
     ) -> Result<Response<CreateNewResponse>, Status> {
-        todo!()
+        let resp = self.create_new_customer(request.into_inner()).await?;
+        Ok(Response::new(CreateNewResponse {
+            customer: Some(resp),
+        }))
     }
 
-    async fn get_all(&self, request: Request<()>) -> Result<Response<GetAllResponse>, Status> {
-        todo!()
+    async fn get_all(&self, _request: Request<()>) -> Result<Response<GetAllResponse>, Status> {
+        let customers = self
+            .customers
+            .lock()
+            .await
+            .into_iter()
+            .map(|i: &mut Pack<customer::Customer>| i.unpack().into())
+            .collect::<Vec<CustomerObj>>();
+        let response = GetAllResponse {
+            customers: customers,
+        };
+        return Ok(Response::new(response));
     }
 
     async fn get_by_id(
         &self,
         request: Request<GetByIdRequest>,
     ) -> Result<Response<GetByIdResponse>, Status> {
-        todo!()
+        let customer: CustomerObj = self
+            .customers
+            .lock()
+            .await
+            .find_id(&request.into_inner().customer_id)
+            .map_err(|_| Status::not_found("Customer not found"))?
+            .unpack()
+            .into();
+        let response = GetByIdResponse {
+            customer: Some(customer),
+        };
+        return Ok(Response::new(response));
     }
 
     async fn update_by_id(
         &self,
         request: Request<UpdateByIdRequest>,
     ) -> Result<Response<UpdateByIdResponse>, Status> {
-        todo!()
+        let _customer: CustomerUpdateObj = match request.into_inner().customer {
+            Some(u) => u,
+            None => return Err(Status::internal("Request has an empty customer object")),
+        };
+        let _address = if let Some(addr) = _customer.address {
+            customer::Address::new(addr.zip, addr.location, addr.address)
+        } else {
+            return Err(ServiceError::internal_error("Missing address from message").into());
+        };
+        let mut lock = self.customers.lock().await;
+        let customer = match lock.find_id_mut(&_customer.id) {
+            Ok(u) => u,
+            Err(err) => return Err(Status::not_found(format!("{}", err))),
+        };
+
+        {
+            let mut customer_mut = customer.as_mut();
+            let mut _customer_mut = customer_mut.unpack();
+            _customer_mut.set_name(_customer.name.to_string());
+            _customer_mut.set_email(_customer.email.to_string())?;
+            _customer_mut.set_phone(_customer.phone.to_string());
+            _customer_mut.set_tax_number(TaxNumber::new(&_customer.tax_number)?);
+            _customer_mut.set_address(_address);
+        }
+
+        let _customer = customer.unpack();
+
+        let response = UpdateByIdResponse {
+            customer: Some(_customer.into()),
+        };
+        return Ok(Response::new(response));
     }
 
     async fn add_user(
