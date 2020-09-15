4c7c384f3c99fff124c83fda7aee084cf3d1c68a
Peter <mezeipetister@gmail.com>
2020-09-15 13:32:56 +0200

    ..

--- src/customer.rs
+++ src/customer.rs
@@ -19,110 +19,108 @@ use crate::prelude::ServiceError::*;
 use crate::prelude::*;
 use crate::taxnumber::*;
 use chrono::prelude::*;
+use packman::*;
 use protos::customer::*;
 use serde::{Deserialize, Serialize};
-use storaget::*;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Customer {
-    id: String,
-    name: String,
-    email: String,
-    phone: String,
-    tax_number: Option<TaxNumber>,
-    address: Address,
-    date_created: DateTime<Utc>,
-    created_by: String,
-    has_user: bool,
-    users: Vec<String>,
+  id: u32,
+  alias: String,
+  name: String,
+  email: String,
+  phone: String,
+  tax_number: Option<TaxNumber>,
+  address: Address,
+  date_created: i64,
+  created_by: u32,
+  has_user: bool,
+  users: Vec<u32>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct Address {
-    zip: String,
-    location: String,
-    address: String,
+  zip: String,
+  location: String,
+  address: String,
 }
 
 impl Address {
-    pub fn new(zip: String, location: String, address: String) -> Self {
-        Self {
-            zip,
-            location,
-            address,
-        }
+  pub fn new(zip: String, location: String, address: String) -> Self {
+    Self {
+      zip,
+      location,
+      address,
     }
+  }
 }
 
 impl From<Customer> for CustomerObj {
-    fn from(u: Customer) -> Self {
-        Self {
-            id: u.id,
-            date_created: Some(protos::prelude::DateTime {
-                rfc_3399: u.date_created.to_rfc3339(),
-            }),
-            created_by: u.created_by,
-            name: u.name,
-            address: Some(protos::customer::Address {
-                zip: u.address.zip,
-                location: u.address.location,
-                address: u.address.address,
-            }),
-            email: u.email,
-            phone: u.phone,
-            tax_number: u.tax_number.unwrap_or_default().into(),
-            has_user: u.has_user,
-            users: u.users,
-        }
+  fn from(u: Customer) -> Self {
+    Self {
+      id: u.id,
+      date_created: u.date_created,
+      created_by: u.created_by,
+      name: u.name,
+      address: Some(protos::customer::Address {
+        zip: u.address.zip,
+        location: u.address.location,
+        address: u.address.address,
+      }),
+      email: u.email,
+      phone: u.phone,
+      tax_number: u.tax_number.unwrap_or_default().into(),
+      has_user: u.has_user,
+      users: u.users,
     }
+  }
 }
 
 impl From<&Customer> for CustomerObj {
-    fn from(u: &Customer) -> Self {
-        let taxnumber: String = match &u.tax_number {
-            Some(number) => number.to_owned().into(),
-            None => "".into(),
-        };
-        Self {
-            id: u.id.to_owned(),
-            date_created: Some(protos::prelude::DateTime {
-                rfc_3399: u.date_created.to_rfc3339(),
-            }),
-            created_by: u.created_by.to_owned(),
-            name: u.name.to_owned(),
-            address: Some(protos::customer::Address {
-                zip: u.address.zip.to_owned(),
-                location: u.address.location.to_owned(),
-                address: u.address.address.to_owned(),
-            }),
-            email: u.email.to_owned(),
-            phone: u.phone.to_owned(),
-            tax_number: taxnumber,
-            has_user: u.has_user,
-            users: u.users.to_owned(),
-        }
+  fn from(u: &Customer) -> Self {
+    let taxnumber: String = match &u.tax_number {
+      Some(number) => number.to_owned().into(),
+      None => "".into(),
+    };
+    Self {
+      id: u.id,
+      date_created: u.date_created,
+      created_by: u.created_by,
+      name: u.name.to_owned(),
+      address: Some(protos::customer::Address {
+        zip: u.address.zip.to_owned(),
+        location: u.address.location.to_owned(),
+        address: u.address.address.to_owned(),
+      }),
+      email: u.email.to_owned(),
+      phone: u.phone.to_owned(),
+      tax_number: taxnumber,
+      has_user: u.has_user,
+      users: u.users.to_owned(),
     }
+  }
 }
 
 impl Default for Customer {
-    fn default() -> Self {
-        Self {
-            id: String::default(),
-            name: String::default(),
-            email: String::default(),
-            phone: String::default(),
-            tax_number: None,
-            address: Address::default(),
-            date_created: Utc::now(),
-            created_by: String::default(),
-            has_user: false,
-            users: Vec::new(),
-        }
+  fn default() -> Self {
+    Self {
+      id: 0,
+      alias: String::default(),
+      name: String::default(),
+      email: String::default(),
+      phone: String::default(),
+      tax_number: None,
+      address: Address::default(),
+      date_created: Utc::now().timestamp_millis(),
+      created_by: 0,
+      has_user: false,
+      users: Vec::new(),
     }
+  }
 }
 
 impl TryFrom for Customer {
-    type TryFrom = Customer;
+  type TryFrom = Customer;
 }
 
 // impl DateCreated for User {
@@ -132,108 +130,115 @@ impl TryFrom for Customer {
 // }
 
 impl Customer {
-    pub fn new(
-        name: String,
-        email: String,
-        phone: String,
-        tax_number: Option<TaxNumber>,
-        address: Address,
-        created_by: String,
-    ) -> ServiceResult<Self> {
-        // Validate Email content
-        if !email.contains('@') || !email.contains('.') {
-            return Err(BadRequest(
-                "Nem megfelelő email cím. Legalább @ jelet és pontot kell tartalmaznia".to_string(),
-            ));
-        }
-        // Validate Name length
-        if name.len() > 200 || name.len() < 2 {
-            return Err(BadRequest(format!(
-                "A név hosszúsága legalább {} max {} karakter",
-                2, 200
-            )));
-        }
-
-        Ok(Self {
-            id: crate::id::generate_alphanumeric(6),
-            name,
-            email,
-            phone,
-            tax_number,
-            address,
-            date_created: Utc::now(),
-            created_by,
-            has_user: false,
-            users: Vec::new(),
-        })
+  pub fn new(
+    id: u32,
+    alias: String,
+    name: String,
+    email: String,
+    phone: String,
+    tax_number: Option<TaxNumber>,
+    address: Address,
+    created_by: u32,
+  ) -> ServiceResult<Self> {
+    // Validate Email content
+    if !email.contains('@') || !email.contains('.') {
+      return Err(BadRequest(
+        "Nem megfelelő email cím. Legalább @ jelet és pontot kell tartalmaznia".to_string(),
+      ));
     }
+    // Validate Name length
+    if name.len() > 200 || name.len() < 2 {
+      return Err(BadRequest(format!(
+        "A név hosszúsága legalább {} max {} karakter",
+        2, 200
+      )));
+    }
+
+    Ok(Self {
+      id,
+      alias,
+      name,
+      email,
+      phone,
+      tax_number,
+      address,
+      date_created: Utc::now().timestamp_millis(),
+      created_by,
+      has_user: false,
+      users: Vec::new(),
+    })
+  }
 }
 
 impl Customer {
-    pub fn get_id(&self) -> &str {
-        &self.id
+  pub fn get_id(&self) -> &u32 {
+    &self.id
+  }
+  pub fn get_alias(&self) -> &str {
+    &self.alias
+  }
+  pub fn get_date_created(&self) -> i64 {
+    self.date_created
+  }
+  pub fn get_name(&self) -> &str {
+    &self.name
+  }
+  pub fn set_name(&mut self, name: String) -> &Self {
+    self.name = name;
+    self
+  }
+  pub fn get_email(&self) -> &str {
+    &self.email
+  }
+  pub fn set_email(&mut self, email: String) -> ServiceResult<&Self> {
+    if email.contains('@') && email.contains('.') && email.len() > 5 {
+      self.email = email;
+      Ok(self)
+    } else {
+      Err(BadRequest(
+        "Rossz email formátum. Legyen legalább 5 karakter, és tartalmazzon @ jelet és pontot"
+          .into(),
+      ))
     }
-    pub fn get_date_created(&self) -> DateTime<Utc> {
-        self.date_created
-    }
-    pub fn get_name(&self) -> &str {
-        &self.name
-    }
-    pub fn set_name(&mut self, name: String) -> &Self {
-        self.name = name;
-        self
-    }
-    pub fn get_email(&self) -> &str {
-        &self.email
-    }
-    pub fn set_email(&mut self, email: String) -> ServiceResult<&Self> {
-        if email.contains('@') && email.contains('.') && email.len() > 5 {
-            self.email = email;
-            Ok(self)
-        } else {
-            Err(BadRequest(
-                    "Rossz email formátum. Legyen legalább 5 karakter, és tartalmazzon @ jelet és pontot"
-                    .into(),
-            ))
-        }
-    }
-    pub fn get_tax_number(&self) -> &Option<TaxNumber> {
-        &self.tax_number
-    }
-    pub fn set_tax_number(&mut self, tax_number: Option<TaxNumber>) -> &Self {
-        self.tax_number = tax_number;
-        self
-    }
-    pub fn get_address(&self) -> &Address {
-        &self.address
-    }
-    pub fn set_address(&mut self, address: Address) -> &Self {
-        self.address = address;
-        self
-    }
-    pub fn get_phone(&self) -> &str {
-        &self.phone
-    }
-    pub fn set_phone(&mut self, phone: String) -> &Self {
-        self.phone = phone;
-        self
-    }
-    pub fn get_created_by(&self) -> &str {
-        &self.created_by
-    }
-    pub fn get_users(&self) -> &Vec<String> {
-        &self.users
-    }
+  }
+  pub fn get_tax_number(&self) -> &Option<TaxNumber> {
+    &self.tax_number
+  }
+  pub fn set_tax_number(&mut self, tax_number: Option<TaxNumber>) -> &Self {
+    self.tax_number = tax_number;
+    self
+  }
+  pub fn get_address(&self) -> &Address {
+    &self.address
+  }
+  pub fn set_address(&mut self, address: Address) -> &Self {
+    self.address = address;
+    self
+  }
+  pub fn get_phone(&self) -> &str {
+    &self.phone
+  }
+  pub fn set_phone(&mut self, phone: String) -> &Self {
+    self.phone = phone;
+    self
+  }
+  pub fn get_created_by(&self) -> u32 {
+    self.created_by
+  }
+  pub fn get_users(&self) -> &Vec<u32> {
+    &self.users
+  }
 }
 
 impl VecPackMember for Customer {
-    fn get_id(&self) -> &str {
-        &self.id
-    }
-    // fn try_from(from: &str) -> StorageResult<Self::ResultType> {
-    //     match deserialize_object(from) {
-    //         Ok(res) => Ok(res),
-    //         Err(_) => Err(ServiceError::DeserializeServiceError("user has wrong format".to_string())),
-    //     }
-    // }
+  type Out = u32;
+  fn get_id(&self) -> &u32 {
+    &self.id
+  }
+  // fn try_from(from: &str) -> StorageResult<Self::ResultType> {
+  //     match deserialize_object(from) {
+  //         Ok(res) => Ok(res),
+  //         Err(_) => Err(ServiceError::DeserializeServiceError("user has wrong format".to_string())),
+  //     }
+  // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,185 +1,302 @@
+// Copyright (C) 2020 peter
+//
+// This file is part of Gardenzilla.
+//
+// Gardenzilla is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Gardenzilla is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gardenzilla.  If not, see <http://www.gnu.org/licenses/>.
+
 pub mod customer;
 pub mod id;
 pub mod prelude;
 pub mod taxnumber;
 
+use packman::*;
 use prelude::*;
 use protos::customer::customer_server::*;
 use protos::customer::*;
 use protos::email::email_client::*;
+use std::collections::HashMap;
 use std::path::PathBuf;
-use storaget::*;
 use taxnumber::*;
 use tokio::sync::{oneshot, Mutex};
 use tonic::transport::Channel;
 use tonic::{transport::Server, Request, Response, Status};
 
+// Customer service
+//
+// Related to manage all customer related
+// tasks. Create, list, update, lookup, etc.
+//
+// Important
+// =========
+// As customer has a key role systemwide,
+// we cannot remove a customer object anyway.
+//
+// Other restrictions
+// ==================
+// Customer ids and aliases are also restricted
+// to update. They auto generated with the customer
+// object, and they remain the same in the future
+
 pub struct CustomerService {
-    customers: Mutex<VecPack<customer::Customer>>,
-    _email_client: Mutex<EmailClient<Channel>>,
+  next_id: Mutex<u32>,                           // Store next available customer ID
+  lookup_table: Mutex<HashMap<String, u32>>,     // Lookup customer id by alias
+  customers: Mutex<VecPack<customer::Customer>>, // Customers db
+  _email_client: Mutex<EmailClient<Channel>>,    // Email service client
 }
 
+// Init customer service
+// Load database, load related service clients
+// set alias lookup table and next id
 impl CustomerService {
-    fn new(
-        customers: Mutex<VecPack<customer::Customer>>,
-        email_client: EmailClient<Channel>,
-    ) -> Self {
-        Self {
-            customers,
-            _email_client: Mutex::new(email_client),
-        }
+  fn init(
+    customers: VecPack<customer::Customer>, // Customers db
+    email_client: EmailClient<Channel>,     // Email service client
+  ) -> CustomerService {
+    // Define the next id
+    // Iterate over the customers
+    // and fold till the biggest id
+    let next_id: u32 = customers.iter().fold(0u32, |prev_id, c| {
+      let id = c.unpack().get_id();
+      if *id > prev_id {
+        // return the current id as
+        // that bigger then the previous one
+        *id
+      } else {
+        // return the previous id
+        prev_id
+      }
+    }) + 1; // biggest found ID + 1 is the next ID
+
+    // Lookup table to store
+    // customer aliases
+    // Store customer alias and customer id
+    //                =====              ==
+    let mut lookup: HashMap<String, u32> = HashMap::new();
+
+    // Build up lookup table
+    // for aliases
+    customers
+      .iter()
+      .map(|c| {
+        let c = c.unpack();
+        (c.get_alias().to_string(), *c.get_id())
+      })
+      .for_each(|o| {
+        let _ = lookup.insert(o.0, o.1);
+      });
+
+    CustomerService {
+      next_id: Mutex::new(next_id),
+      lookup_table: Mutex::new(lookup),
+      customers: Mutex::new(customers),
+      _email_client: Mutex::new(email_client),
     }
-    async fn create_new_customer(&self, u: CreateNewRequest) -> ServiceResult<CustomerObj> {
-        let taxnumber = match u.tax_number.len() {
-            x if x > 0 => Some(TaxNumber::new(&u.tax_number)?),
-            _ => None,
-        };
-        let new_customer = customer::Customer::new(
-            u.name,
-            u.email,
-            u.phone,
-            taxnumber,
-            customer::Address::new(u.zip, u.location, u.address),
-            u.created_by,
-        )?;
-        let customer_obj: CustomerObj = (&new_customer).into();
-        self.customers.lock().await.insert(new_customer)?;
-        Ok(customer_obj)
+  }
+
+  async fn create_new_customer(&self, u: CreateNewRequest) -> ServiceResult<CustomerObj> {
+    // Check taxnumber
+    let taxnumber = match u.tax_number.len() {
+      x if x > 0 => Some(TaxNumber::new(&u.tax_number)?),
+      _ => None,
+    };
+
+    let mut alias: String;
+
+    // Generate customer alias
+    // Using a loop, generate random IDs till
+    // we find an available ID
+    loop {
+      alias = id::generate_alphanumeric(7);
+      if self.is_alias_available(&alias).await {
+        break;
+      }
     }
+
+    // Create customer object
+    let new_customer = customer::Customer::new(
+      *self.next_id.lock().await, // Get next free ID
+      alias,                      // Generate auto alias
+      u.name,
+      u.email,
+      u.phone,
+      taxnumber,
+      customer::Address::new(u.zip, u.location, u.address),
+      u.created_by,
+    )?;
+
+    // Increment next ID
+    *self.next_id.lock().await += 1;
+
+    let customer_obj: CustomerObj = (&new_customer).into();
+
+    // Store new customer in DB
+    self.customers.lock().await.insert(new_customer)?;
+
+    // Returns customer proto object
+    Ok(customer_obj)
+  }
+
+  // Check wheter an alias is available
+  // or already taken
+  async fn is_alias_available(&self, alias: &str) -> bool {
+    self.lookup_table.lock().await.get(alias).is_some()
+  }
 }
 
 #[tonic::async_trait]
 impl Customer for CustomerService {
-    async fn create_new(
-        &self,
-        request: Request<CreateNewRequest>,
-    ) -> Result<Response<CreateNewResponse>, Status> {
-        let resp = self.create_new_customer(request.into_inner()).await?;
-        Ok(Response::new(CreateNewResponse {
-            customer: Some(resp),
-        }))
+  async fn create_new(
+    &self,
+    request: Request<CreateNewRequest>,
+  ) -> Result<Response<CreateNewResponse>, Status> {
+    let resp = self.create_new_customer(request.into_inner()).await?;
+    Ok(Response::new(CreateNewResponse {
+      customer: Some(resp),
+    }))
+  }
+
+  // Todo!: this should be a response stream
+  //                        !===============
+  async fn get_all(&self, _request: Request<()>) -> Result<Response<GetAllResponse>, Status> {
+    let customers = self
+      .customers
+      .lock()
+      .await
+      .into_iter()
+      .map(|i: &mut Pack<customer::Customer>| i.unpack().into())
+      .collect::<Vec<CustomerObj>>();
+    let response = GetAllResponse {
+      customers: customers,
+    };
+    return Ok(Response::new(response));
+  }
+
+  async fn get_by_id(
+    &self,
+    request: Request<GetByIdRequest>,
+  ) -> Result<Response<GetByIdResponse>, Status> {
+    let customer: CustomerObj = self
+      .customers
+      .lock()
+      .await
+      .find_id(&request.into_inner().customer_id)
+      .map_err(|_| Status::not_found("Customer not found"))?
+      .unpack()
+      .into();
+    let response = GetByIdResponse {
+      customer: Some(customer),
+    };
+    return Ok(Response::new(response));
+  }
+
+  async fn update_by_id(
+    &self,
+    request: Request<UpdateByIdRequest>,
+  ) -> Result<Response<UpdateByIdResponse>, Status> {
+    let _customer: CustomerUpdateObj = match request.into_inner().customer {
+      Some(u) => u,
+      None => return Err(Status::internal("Request has an empty customer object")),
+    };
+    let taxnumber = match _customer.tax_number.len() {
+      x if x > 0 => Some(TaxNumber::new(&_customer.tax_number)?),
+      _ => None,
+    };
+    let _address = if let Some(addr) = _customer.address {
+      customer::Address::new(addr.zip, addr.location, addr.address)
+    } else {
+      return Err(ServiceError::internal_error("Missing address from message").into());
+    };
+    let mut lock = self.customers.lock().await;
+    let customer = match lock.find_id_mut(&_customer.id) {
+      Ok(u) => u,
+      Err(err) => return Err(Status::not_found(format!("{}", err))),
+    };
+
+    {
+      let mut customer_mut = customer.as_mut();
+      let mut _customer_mut = customer_mut.unpack();
+      _customer_mut.set_name(_customer.name.to_string());
+      _customer_mut.set_email(_customer.email.to_string())?;
+      _customer_mut.set_phone(_customer.phone.to_string());
+      _customer_mut.set_tax_number(taxnumber);
+      _customer_mut.set_address(_address);
     }
 
-    async fn get_all(&self, _request: Request<()>) -> Result<Response<GetAllResponse>, Status> {
-        let customers = self
-            .customers
-            .lock()
-            .await
-            .into_iter()
-            .map(|i: &mut Pack<customer::Customer>| i.unpack().into())
-            .collect::<Vec<CustomerObj>>();
-        let response = GetAllResponse {
-            customers: customers,
-        };
-        return Ok(Response::new(response));
-    }
+    let _customer = customer.unpack();
 
-    async fn get_by_id(
-        &self,
-        request: Request<GetByIdRequest>,
-    ) -> Result<Response<GetByIdResponse>, Status> {
-        let customer: CustomerObj = self
-            .customers
-            .lock()
-            .await
-            .find_id(&request.into_inner().customer_id)
-            .map_err(|_| Status::not_found("Customer not found"))?
-            .unpack()
-            .into();
-        let response = GetByIdResponse {
-            customer: Some(customer),
-        };
-        return Ok(Response::new(response));
-    }
+    let response = UpdateByIdResponse {
+      customer: Some(_customer.into()),
+    };
+    return Ok(Response::new(response));
+  }
 
-    async fn update_by_id(
-        &self,
-        request: Request<UpdateByIdRequest>,
-    ) -> Result<Response<UpdateByIdResponse>, Status> {
-        let _customer: CustomerUpdateObj = match request.into_inner().customer {
-            Some(u) => u,
-            None => return Err(Status::internal("Request has an empty customer object")),
-        };
-        let taxnumber = match _customer.tax_number.len() {
-            x if x > 0 => Some(TaxNumber::new(&_customer.tax_number)?),
-            _ => None,
-        };
-        let _address = if let Some(addr) = _customer.address {
-            customer::Address::new(addr.zip, addr.location, addr.address)
-        } else {
-            return Err(ServiceError::internal_error("Missing address from message").into());
-        };
-        let mut lock = self.customers.lock().await;
-        let customer = match lock.find_id_mut(&_customer.id) {
-            Ok(u) => u,
-            Err(err) => return Err(Status::not_found(format!("{}", err))),
-        };
+  async fn add_user(
+    &self,
+    _request: Request<AddUserRequest>,
+  ) -> Result<Response<AddUserResponse>, Status> {
+    todo!()
+  }
 
-        {
-            let mut customer_mut = customer.as_mut();
-            let mut _customer_mut = customer_mut.unpack();
-            _customer_mut.set_name(_customer.name.to_string());
-            _customer_mut.set_email(_customer.email.to_string())?;
-            _customer_mut.set_phone(_customer.phone.to_string());
-            _customer_mut.set_tax_number(taxnumber);
-            _customer_mut.set_address(_address);
-        }
+  async fn remove_user(
+    &self,
+    _request: Request<RemoveUserRequest>,
+  ) -> Result<Response<RemoveUserResponse>, Status> {
+    todo!()
+  }
 
-        let _customer = customer.unpack();
-
-        let response = UpdateByIdResponse {
-            customer: Some(_customer.into()),
-        };
-        return Ok(Response::new(response));
-    }
-
-    async fn add_user(
-        &self,
-        _request: Request<AddUserRequest>,
-    ) -> Result<Response<AddUserResponse>, Status> {
-        todo!()
-    }
-
-    async fn remove_user(
-        &self,
-        _request: Request<RemoveUserRequest>,
-    ) -> Result<Response<RemoveUserResponse>, Status> {
-        todo!()
-    }
+  async fn lookup(
+    &self,
+    _request: Request<LookupRequest>,
+  ) -> Result<Response<LookupResponse>, Status> {
+    todo!()
+  }
 }
 
 #[tokio::main]
 async fn main() -> prelude::ServiceResult<()> {
-    let customers: Mutex<VecPack<customer::Customer>> = Mutex::new(
-        VecPack::try_load_or_init(PathBuf::from("data/customers"))
-            .expect("Error while loading customers storage"),
-    );
+  // Load customers db
+  let customers: VecPack<customer::Customer> =
+    VecPack::try_load_or_init(PathBuf::from("data/customers"))
+      .expect("Error while loading customers storage");
 
-    let email_client = EmailClient::connect("http://[::1]:50053")
-        .await
-        .expect("Error while connecting to email service");
+  // Connect to email service
+  let email_client = EmailClient::connect("http://[::1]:50053")
+    .await
+    .expect("Error while connecting to email service");
 
-    let customer_service = CustomerService::new(customers, email_client);
+  // Init customer service
+  let customer_service = CustomerService::init(customers, email_client);
 
-    let addr = "[::1]:50055".parse().unwrap();
+  let addr = "[::1]:50055".parse().unwrap();
 
-    // Create shutdown channel
-    let (tx, rx) = oneshot::channel();
+  // Create shutdown channel
+  let (tx, rx) = oneshot::channel();
 
-    // Spawn the server into a runtime
-    tokio::task::spawn(async move {
-        Server::builder()
-            .add_service(CustomerServer::new(customer_service))
-            .serve_with_shutdown(addr, async { rx.await.unwrap() })
-            .await
-    });
+  // Spawn the server into a runtime
+  tokio::task::spawn(async move {
+    Server::builder()
+      .add_service(CustomerServer::new(customer_service))
+      .serve_with_shutdown(addr, async { rx.await.unwrap() })
+      .await
+  });
 
-    tokio::signal::ctrl_c().await.unwrap();
+  tokio::signal::ctrl_c().await.unwrap();
 
-    println!("SIGINT");
+  println!("SIGINT");
 
-    // Send shutdown signal after SIGINT received
-    let _ = tx.send(());
+  // Send shutdown signal after SIGINT received
+  let _ = tx.send(());
 
-    Ok(())
+  Ok(())
 }
